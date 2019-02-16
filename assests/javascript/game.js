fe6cdc0e5f42a13f772675a4653817abb0031273
Jad El Ramadi <jrman523@users.noreply.github.com>
2019-02-16 17:40:01 -0500

    using find to get value test

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
     var defender = $("#defender");
     var Ahealth = 0;
     var Dhealth = 0;
+    var attackA = 0;
+    var attackB = 0;
     var attackerCheck = false;
     var defenderCheck = false;
     $('.img').click(function () {
@@ -27,7 +29,7 @@ $(document).ready(function () {
         if (defenderCheck) {
             if ($(this).hasClass('img-enemy')) {
                 $(this).addClass('img-defender');
-
+                defenderCheck = false;
                 $('.img').each(function (index) {
                     if ($(this).hasClass('img-defender')) {
                         defender.prepend($(this));
@@ -36,5 +38,10 @@ $(document).ready(function () {
                 });
             }
         }
+    });
+
+    $('#attack').click(function attack() {
+        var test = $('.img-attacker').find('p').val();
+        console.log(parseInt(test));
     });
 });
