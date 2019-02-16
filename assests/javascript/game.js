19f6ed615d86e4278e3c1a045728521c95c806cc
Jad El Ramadi <jadelramadi@gmail.com>
2019-02-16 09:39:03 -0500

    added pictures and finshed html and css layouts

---
+++ assests/javascript/game.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    var pick = $("#pick");
+    var enemy = $("#enemy");
+    var attacker = $("attacker");
+    var defender = $("defender");
+
+   ('.img').click(function(){
+    $(this).addClass('img-attacker');
+    pick();
+   });
+
+   function pick(){
+    if ($('.img').hasClass('img-attacker')){
+        $('.img-attacker').append(attacker);
+    }else{
+        $('.img').append(enemy);
+        $('.img').addClass('img-enemy');
+    }
+   }
+
+   ('.img-enemy').click(function(){
+    $(this).removeClass("img-enemy");
+    $(this).addClass('img-defender');
+    $(this).append(defender);
+   });
+});
